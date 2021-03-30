200258f3dc32b2494cc0cb0ceaa0fd6547a06eb3
Jaeyah <seongjae9611@daum.net>
2021-03-31 03:58:06 +0900

    no message

--- text_10/JS/main.js
+++ text_10/JS/main.js
@@ -17,6 +17,7 @@ searchInputEl.addEventListener('blur', function(){
 });
 
 const badgeEl = document.querySelector('header .badges');
+const toTopEl = document.querySelector('#to_top');
 
 window.addEventListener('scroll',_.throttle(function() {
     console.log(window.scrollY);
@@ -27,15 +28,31 @@ window.addEventListener('scroll',_.throttle(function() {
             opacity: 0,
             display: 'none'
         });
+        // 버튼 보이기
+        gsap.to(toTopEl, .2, {
+            x: 0
+        });
     } else {
         // 배지 보이기
         gsap.to(badgeEl, .6, {
             opacity: 1,
             display: 'block'
         });
+        // 버튼 숨기기
+        gsap.to(toTopEl, .2, {
+            x: 100
+        });
     }
 }, 300));
 // _.throttle(함수, 시간)
+
+
+toTopEl.addEventListener('click', function() {
+    gsap.to(window, .7, {
+        scrollTo: 0
+    });
+});
+
 
 
 const fadeEls = document.querySelectorAll('.visual .fade_in');
@@ -74,6 +91,20 @@ new Swiper('.promotion .swiper-container', {
     }
 });
 
+new Swiper('.awards .swiper-container', {
+    direction: 'horizontal',
+    autoplay: true,
+    loop: true,
+    spaceBetween: 30,
+    slidesPerView: 5,
+    navigation: {
+        prevEl: '.awards .swiper-prev',
+        nextEl: '.awards .swiper-next'
+    }
+});
+
+
+
 const promotionEl = document.querySelector('.promotion');
 const promotionToggleBtn = document.querySelector('.toggle_promotion');
 let isHidePromotion = false;
@@ -111,3 +142,18 @@ function floatingObject(selector, delay, size) {
 floatingObject('.floating1', 1, 15);
 floatingObject('.floating2', .5, 15);
 floatingObject('.floating3', 1.5, 20);
+
+const spyEls=document.querySelectorAll('section.scroll-spy');
+spyEls.forEach(function(spyEl) {
+    new ScrollMagic
+    .Scene({
+        triggerElement: spyEl, // 보여짐 여부를 감시할 요소
+        triggerHook: .8
+    })
+    .setClassToggle(spyEl, 'show')
+    .addTo(new ScrollMagic.Controller());
+});
+
+
+const thisYear = document.querySelector('.this_year');
+thisYear.textContent = new Date().getFullYear(); // 올해의 년도를 말함
